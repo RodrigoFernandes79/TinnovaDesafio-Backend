768255a8a472668cd5465154041f2652fbeba719
RodrigoFernandes79 <rodrigohf79@hotmail.com>
2021-10-27 20:31:57 -0300

    Criando Entidade Veículo;

---
+++ src/main/java/com/tinnova/ex05controleDeVeiculos/Ex05ControleDeVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.tinnova.ex05controleDeVeiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex05ControleDeVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex05ControleDeVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinnova/ex05controleDeVeiculos/Ex05ControleDeVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinnova.ex05controleDeVeiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex05ControleDeVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
