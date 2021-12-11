d414ae810302486aac8f64960787fe20b488db82
RodrigoFernandes79 <rodrigohf79@hotmail.com>
2021-12-11 19:21:14 -0300

    Criando Annotation para integração com FrontEnd

--- src/main/java/com/tinnova/ex05controleDeVeiculos/resource/VeiculoResource.java
+++ src/main/java/com/tinnova/ex05controleDeVeiculos/resource/VeiculoResource.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -24,6 +25,7 @@ import com.tinnova.ex05controleDeVeiculos.service.VeiculoService;
 
 @RestController
 @RequestMapping("/veiculos")
+@CrossOrigin("http://localhost:4200")
 public class VeiculoResource {
 
 	@Autowired
